c0eda213b14891af9643cca756a6537c9f115454
“Sylvanus <sylvanusuzor@gmail.com>
2024-04-07 08:05:26 -0700

    correction

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -17,8 +17,8 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
+        if (i % 3) == 0 and (i % 5) == 0:  # Corrected the order of conditions
+            tmp_result.append("FizzBuzz")   # Moved this condition to the top
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
         elif (i % 5) == 0:
